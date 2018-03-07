0f8db8132216883564548549e33fd5b31206503b
bastienwcs <bastien@wildcodeschool.fr>
2018-03-07 16:10:35 +0100

    ScrollView et chanrgement Drawable en Java

--- app/src/main/java/fr/wcs/wildtweet/MainActivity.java
+++ app/src/main/java/fr/wcs/wildtweet/MainActivity.java
@@ -1,6 +1,8 @@
 package fr.wcs.wildtweet;
 
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -8,6 +10,7 @@ import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -20,6 +23,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView imageLogo = findViewById(R.id.image_logo);
+        Drawable drawableLogo = ContextCompat.getDrawable(this, R.drawable.wildtweetlogo);
+        imageLogo.setImageDrawable(drawableLogo);
 
         final Button buttonLogin = findViewById(R.id.button_login);
         buttonLogin.setOnClickListener(new View.OnClickListener() {
