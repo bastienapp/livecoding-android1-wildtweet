c39c6030221aff25442daa71e05d765f33cf7308
bastienwcs <bastien@wildcodeschool.fr>
2018-03-07 16:11:28 +0100

    rebase

--- app/src/main/java/fr/wcs/wildtweet/MainActivity.java
+++ app/src/main/java/fr/wcs/wildtweet/MainActivity.java
@@ -11,7 +11,6 @@ import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.ImageView;
-import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
