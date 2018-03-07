b42809518e8d2b4eba3443298006222fa8f10c81
bastienwcs <bastien@wildcodeschool.fr>
2018-03-07 15:34:55 +0100

    Activity ListrTweet et changement de page

---
+++ app/src/main/java/fr/wcs/wildtweet/ListTweetActivity.java
@@ -0,0 +1,13 @@
+package fr.wcs.wildtweet;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ListTweetActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_tweet);
+    }
+}
