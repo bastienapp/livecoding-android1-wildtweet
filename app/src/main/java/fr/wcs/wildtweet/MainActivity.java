b9ca87eb386dc643477c337414df0287d73c9b6b
bastienwcs <bastien@wildcodeschool.fr>
2018-03-21 12:08:36 +0100

    fix ergonomie

--- app/src/main/java/fr/wcs/wildtweet/MainActivity.java
+++ app/src/main/java/fr/wcs/wildtweet/MainActivity.java
@@ -1,31 +1,24 @@
 package fr.wcs.wildtweet;
 
 import android.content.Intent;
-import android.graphics.drawable.Drawable;
-import android.support.v4.content.ContextCompat;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.EditText;
-import android.widget.ImageView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    public static String EXTRA_PASSWORD = "EXTRA_PASSWORD";
     public static String EXTRA_LOGIN = "EXTRA_LOGIN";
+    public static String EXTRA_PASSWORD = "EXTRA_PASSWORD";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        ImageView imageLogo = findViewById(R.id.image_logo);
-        Drawable drawableLogo = ContextCompat.getDrawable(this, R.drawable.wildtweetlogo);
-        imageLogo.setImageDrawable(drawableLogo);
 
         final Button buttonLogin = findViewById(R.id.button_login);
         buttonLogin.setOnClickListener(new View.OnClickListener() {
@@ -50,11 +43,6 @@ public class MainActivity extends AppCompatActivity {
         checkCGU.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
-                EditText editLogin = findViewById(R.id.edit_password);
-                EditText editPassword = findViewById(R.id.edit_login);
-
-                editLogin.setEnabled(isChecked);
-                editPassword.setEnabled(isChecked);
                 buttonLogin.setEnabled(isChecked);
             }
         });
